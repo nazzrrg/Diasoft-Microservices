6a01a204bed7d5ba5320885e8c483f691a806dfe
Egor Nazarov <nazarov.egor2013@gmail.com>
2021-10-24 18:04:06 +0300

    homework 6

--- hw1/src/main/java/com/example/hw1/GreetingService.java
+++ hw1/src/main/java/com/example/hw1/GreetingService.java
@@ -1,10 +1,13 @@
 package com.example.hw1;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional(propagation = Propagation.REQUIRED)
 public class GreetingService {
     private final GreetingRepository greetingRepository;
 
@@ -12,22 +15,27 @@ public class GreetingService {
         this.greetingRepository = greetingRepository;
     }
 
+    @Transactional
     public void save(Greeting greeting) {
         greetingRepository.save(greeting);
     }
 
+    @Transactional(readOnly = true)
     public List<Greeting> getAll() {
         return (List<Greeting>) greetingRepository.findAll();
     }
 
+    @Transactional(readOnly = true)
     public Greeting getById(Long id) {
         return greetingRepository.findById(id).orElseThrow(ElementNotFoundException::new);
     }
 
+    @Transactional
     public void delete(Long id) {
         greetingRepository.deleteById(id);
     }
 
+    @Transactional
     public void update(Greeting greeting) {
         save(greeting);
     }
